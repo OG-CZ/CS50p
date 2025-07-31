1f292f100308c5f754fac56016745f80638f6254
og-cz <16796127+og-cz@users.noreply.github.com>
2025-07-31 23:59:09 +0800

    lectures done

---
+++ week1/_LECTURES/01_if_compare.py
@@ -0,0 +1,19 @@
+a = 10
+b = 12
+
+
+if a < b:
+    print("a is less than b")
+
+a = 14
+b = 12
+
+if a > b:
+    print("a is greater than b")
+
+
+a = 14
+b = 14
+
+if a == b:
+    print(f"{a} is equals to {b}")
